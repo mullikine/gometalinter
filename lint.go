3e28b9f897b1fdb4ae0b316d6bb45f33bc569be8
Shane Mulligan <mullikine@gmail.com>
2018-10-10 17:10:31 +1300

    added toekn

--- lint.go
+++ lint.go
@@ -8,6 +8,7 @@ import (
 	"os/exec"
 	"go/ast"
 	"os/user"
+	"go/token"
 	"go/parser"
 	"go/types"
 	"path/filepath"
@@ -219,6 +220,19 @@ type pkg struct {
 	main bool
 
 	problems []Problem
+}
+
+type Problem struct {
+	Position   token.Position // position in source file
+	Text       string         // the prose that describes the problem
+	Link       string         // (optional) the link to the style guide for the problem
+	Confidence float64        // a value in (0,1] estimating the confidence in this problem's correctness
+	LineText   string         // the source line
+	Category   string         // a short name for the general category of the problem
+
+	// If the problem has a suggested fix (the minority case),
+	// ReplacementLine is a full replacement for the relevant line of the source file.
+	ReplacementLine string
 }
 
 func (p *pkg) lint() []Problem {
